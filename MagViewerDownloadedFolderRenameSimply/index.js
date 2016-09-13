7eebd3a591ee5d58fc3572b6c7c4c38bad076112
110EIm <dreamaker7770@gmail.com>
2016-09-13 21:54:38 +0900

    Better

--- MagViewerDownloadedFolderRenameSimply/index.js
+++ MagViewerDownloadedFolderRenameSimply/index.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 fs.readdir('.', (err, files) => {
 	files.forEach(file => {
-		let newPath = file.replace(/^\d{5,}/, '').replace(/^\s/, '');
+		if(file.split(' ').length === 1) return;
+		let newPath = file.replace(/^\d{1,}/, '').replace(/^\s/, '');
 		if(file === newPath) return;
 		fs.rename(file, newPath, () => { // '304094 도쿄구울 re 6화' to '도쿄구울 re 6화'
 			console.log(`Change filename ${file} to ${newPath}`);
